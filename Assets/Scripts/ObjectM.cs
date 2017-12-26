0e75135c5ad5674a151b66095b2de7af6ed4b3f5
sharkmanch <danteman1992@gmail.com>
2017-12-26 23:21:14 +0800

    first commit

---
+++ Assets/Scripts/ObjectM.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectM : MonoBehaviour {
+
+	[SerializeField] float objectSpeed = 1;
+	private float resetPosition = -23.50f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.left * (objectSpeed * Time.deltaTime));
+
+		if (transform.localPosition.x <= resetPosition) {
+			Vector3 newPos = new Vector3 (130, transform.position.y, transform.position.z);
+			transform.position = newPos;
+		
+		}
+
+
+
+	}
+}
